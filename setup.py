aad2faa77d922c678b921597cb147bb9b42a93f6
Ross Reedstrom <reedstrm@gmail.com>
2016-03-17 12:02:01 -0500

    correct dependencies. Add tests support

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
+"""
+Copyright (C) 2016 Rice University
+
+This software is subject to the provisions of the
+GNU AFFERO GENERAL PUBLIC LICENSE Version 3.0 (AGPL).
+See LICENSE.txt for details.
+"""
 from setuptools import setup, find_packages
 
 install_requires = (
-    'cssselect',
     'cssselect2',
     'tinycss2',
     'lxml',
-    'tinycss',
     )
 
 setup(
@@ -18,6 +23,7 @@ setup(
     license='LGPL, See also LICENSE.txt',
     description='',
     packages=find_packages(),
+    test_suite='cnxeasybake.tests',
     install_requires=install_requires,
     include_package_data=True,
     entry_points={
@@ -25,5 +31,6 @@ setup(
             'cnx-easybake = cnxeasybake.scripts.main:main',
             ],
         },
-    dependency_links=['git+https://github.com/SimonSapin/cssselect2.git#egg=cssselect2']
+    dependency_links=[
+        'git+https://github.com/SimonSapin/cssselect2.git#egg=cssselect2']
     )
