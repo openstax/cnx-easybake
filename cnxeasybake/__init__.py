0afef2483dfefc7ae76c6f0ff220267f6b4a266c
Ross Reedstrom <reedstrm@gmail.com>
2016-03-17 12:06:35 -0500

    inits and fodder for building tests

---
+++ cnxeasybake/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# ###
+# Copyright (c) 2016, Rice University
+# This software is subject to the provisions of the GNU Affero General
+# Public License version 3 (AGPLv3).
+# See LICENCE.txt for details.
+# ###
+from .oven import Oven  # noqa
---
+++ cnxeasybake/tests/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# ###
+# Copyright (c) 2016, Rice University
+# This software is subject to the provisions of the GNU Affero General
+# Public License version 3 (AGPLv3).
+# See LICENCE.txt for details.
+# ###
