bb22b8845cdd1880c2a28c0d4f0eb21f421dab2e
Ross Reedstrom <reedstrm@gmail.com>
2016-04-15 14:41:37 -0500

    test updater now reports diffs in expected html

--- cnxeasybake/tests/update-log-less.sh
+++ cnxeasybake/tests/update-log-less.sh
@@ -7,6 +7,12 @@ for r in rulesets/${rulename}.less
         name=${file%%.less}
         echo $name
         lessc rulesets/${name}.less rulesets/${name}.css
-        cnx-easybake -v rulesets/${name}.css html/${name}_raw.html html/${name}_cooked.html 2>&1 | sed 's;^;// LOG: ;' >> rulesets/${name}.less
+        cnx-easybake -v rulesets/${name}.css html/${name}_raw.html html/${name}_cooked.html.tmp 2>&1 | sed 's;^;// LOG: ;' >> rulesets/${name}.less
+        if ! cmp html/${name}_cooked.html{,.tmp};  then
+            diff -u html/${name}_cooked.html{.tmp,}
+            mv html/${name}_cooked.html{.tmp,}
+        else
+            rm html/${name}_cooked.html.tmp
+        fi
         touch rulesets/${name}.css
     done  
