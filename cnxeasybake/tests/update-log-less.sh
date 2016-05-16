de26fb37193de48f7725997feb4840187170f62b
Ross Reedstrom <reedstrm@gmail.com>
2016-05-16 15:01:52 -0500

    convert string processing to use TargetVal, to support target-*() functions

--- cnxeasybake/tests/update-log-less.sh
+++ cnxeasybake/tests/update-log-less.sh
@@ -7,7 +7,7 @@ for r in rulesets/${rulename}.less
         name=${file%%.less}
         echo $name
         lessc rulesets/${name}.less rulesets/${name}.css
-        cnx-easybake -v rulesets/${name}.css html/${name}_raw.html html/${name}_cooked.html.tmp 2>&1 | sed 's;^;// LOG: ;' >> rulesets/${name}.less
+        cnx-easybake -d rulesets/${name}.css html/${name}_raw.html html/${name}_cooked.html.tmp 2>&1 | sed 's;^;// LOG: ;' >> rulesets/${name}.less
         if ! cmp html/${name}_cooked.html{,.tmp};  then
             diff -u html/${name}_cooked.html{.tmp,}
             mv html/${name}_cooked.html{.tmp,}
