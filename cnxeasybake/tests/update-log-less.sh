f2877246f6962386d8ded3e462903a72433b997b
Ross Reedstrom <reedstrm@gmail.com>
2016-05-18 11:24:59 -0500

    put log output with html output

--- cnxeasybake/tests/update-log-less.sh
+++ cnxeasybake/tests/update-log-less.sh
@@ -6,7 +6,7 @@ for r in rulesets/${rulename}.less
         name=${file%%.less}
         echo $name
         lessc rulesets/${name}.less rulesets/${name}.css
-        cnx-easybake -d rulesets/${name}.css html/${name}_raw.html html/${name}_cooked.html.tmp 2> rulesets/${name}.log
+        cnx-easybake -d rulesets/${name}.css html/${name}_raw.html html/${name}_cooked.html.tmp 2> html/${name}.log
         if ! cmp html/${name}_cooked.html{,.tmp};  then
             diff -u html/${name}_cooked.html{.tmp,}
             mv html/${name}_cooked.html{.tmp,}
